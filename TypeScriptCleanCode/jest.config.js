848f72c0a8b27a2694618dcc4562480318648ef9
Pedro Costa <pedrocostaugusto@gmail.com>
2023-07-26 09:39:30 -0300

    chore: add jest to lint-staged

---
+++ TypeScriptCleanCode/jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('jest').Config} */
+const config = {
+    roots: ["<rootDir>/src"],
+    collectCoverage: true,
+    collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+    coverageDirectory: "coverage",
+    testEnvironment: "node",
+    transform: {
+        ".+.ts$": "ts-jest",
+    },
+};
+
+module.exports = config;
