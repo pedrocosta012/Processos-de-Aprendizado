8d1a734c40718b732c66b666f41dfcb981536dc9
Pedro Costa <pedrocostaugusto@gmail.com>
2023-08-02 07:55:54 -0300

    test: ensure SignUpController calls AddAccount with correct values

--- TypeScriptCleanCode/tests/presentation/controllers/signUpController.test.ts
+++ TypeScriptCleanCode/tests/presentation/controllers/signUpController.test.ts
@@ -1,11 +1,23 @@
+import { AccountModel } from '../../../src/domain/models/account';
+import { AddAccount, AddAccountModel } from '../../../src/domain/usecases/addAccount';
 import { SignUpController } from '../../../src/presentation/controllers/signUpController';
 import { InvalidParamError, MissingParamError, ServerError } from '../../../src/presentation/errors';
 import { Controller, EmailValidator } from '../../../src/presentation/protocols';
 
-interface SutTypes {
-    sut: Controller
-    emailValidatorStub: EmailValidator
-}
+const makeAddAccount = (): AddAccount => {
+    class AddAccountStub implements AddAccount {
+        add(account: AddAccountModel): AccountModel {
+            const fakeAccount = {
+                id: 'valid_id',
+                name: 'valid_name',
+                email: 'valid_email@mail.com',
+                password: 'valid_password'
+            };
+            return fakeAccount;
+        }
+    }
+    return new AddAccountStub();
+};
 
 const makeEmailValidator = (): EmailValidator => {
     class EmailValidatorStub implements EmailValidator {
@@ -16,12 +28,20 @@ const makeEmailValidator = (): EmailValidator => {
     return new EmailValidatorStub();
 };
 
+interface SutTypes {
+    sut: Controller
+    emailValidatorStub: EmailValidator
+    addAccountStub: AddAccount
+}
+
 const makeSut = (): SutTypes => {
     const emailValidatorStub = makeEmailValidator();
-    const sut = new SignUpController(emailValidatorStub);
+    const addAccountStub = makeAddAccount();
+    const sut = new SignUpController(emailValidatorStub, addAccountStub);
     return {
         sut,
-        emailValidatorStub
+        emailValidatorStub,
+        addAccountStub
     };
 };
 
@@ -151,5 +171,25 @@ describe('SignUpController', () => {
 
         expect(httpResponse.statusCode).toBe(400);
         expect(httpResponse.body).toEqual(new InvalidParamError('passwordConfirmation'));
+    });
+
+    it('Should call AddAccount with correct values', () => {
+        const { sut, addAccountStub } = makeSut();
+        const addSpy = jest.spyOn(addAccountStub, 'add');
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'any_email@mail.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+            }
+        };
+        sut.handle(httpRequest);
+
+        expect(addSpy).toHaveBeenCalledWith({
+            name: 'any_name',
+            email: 'any_email@mail.com',
+            password: 'any_password'
+        });
     });
 });
