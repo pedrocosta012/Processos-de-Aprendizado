29107be9583f3582f64a0a9edacd295567492e7b
Pedro Costa <pedrocostaugusto@gmail.com>
2023-08-02 07:30:14 -0300

    feat: ensure SignUpController returns 400 if password confirmation fails

--- TypeScriptCleanCode/src/presentation/controllers/signUpController.ts
+++ TypeScriptCleanCode/src/presentation/controllers/signUpController.ts
@@ -21,6 +21,9 @@ export class SignUpController implements Controller {
             if (!emailIsValid) {
                 return badRequest(new InvalidParamError('email'));
             }
+            if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+                return badRequest(new InvalidParamError('passwordConfirmation'));
+            }
         } catch (error) {
             return serverError();
         }
