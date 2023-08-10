7f35752b8cc331807ddb893bef89affc372dca21
Pedro Costa <pedrocostaugusto@gmail.com>
2023-08-10 10:34:15 -0300

    refactor: connect to mongodb before integration tests

--- TypeScriptCleanCode/tests/main/routes/signUp.test.ts
+++ TypeScriptCleanCode/tests/main/routes/signUp.test.ts
@@ -1,8 +1,22 @@
 import request from 'supertest';
 import app from '../../../src/main/config/app';
 import { faker } from '@faker-js/faker';
+import { mongoHelper } from '../../infra/db/mongodb/helper/mongoHelper';
 
 describe('SignUp Routes', () => {
+    beforeAll(async () => {
+        await mongoHelper.connect(`mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/jest-${process.env.MONGO_DATABASE}`);
+    });
+
+    afterAll(async () => {
+        await mongoHelper.disconnect();
+    });
+
+    beforeEach(async () => {
+        const accountCollection = mongoHelper.getCollection('account');
+        await accountCollection.deleteMany({});
+    });
+
     test('Should enable CORS', async () => {
         const fakeUserPassword = faker.internet.password();
         await request(app).post('/signup').send({
