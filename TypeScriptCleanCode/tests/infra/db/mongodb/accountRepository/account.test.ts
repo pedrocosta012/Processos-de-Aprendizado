17df75b205027fd7b0c75c6fc5737587baf43fcc
Pedro Costa <pedrocostaugusto@gmail.com>
2023-08-10 08:41:04 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ TypeScriptCleanCode/tests/infra/db/mongodb/accountRepository/account.test.ts
@@ -0,0 +1,28 @@
+import { AccountMongoRepository } from '../../../../../src/infra/db/mongodb/accountRepository/account';
+import { mongoHelper } from '../helper/mongoHelper';
+import 'dotenv/config';
+
+describe('Account Mongo Repository', () => {
+    beforeAll(async () => {
+        await mongoHelper.connect(`mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/jest-${process.env.MONGO_DATABASE}`);
+    });
+
+    afterAll(async () => {
+        await mongoHelper.disconnect();
+    });
+
+    it('Should return an account on success', async () => {
+        const sut = new AccountMongoRepository();
+        const account = await sut.add({
+            name: 'any_name',
+            email: 'any_email@mail.com',
+            password: 'any_password'
+        });
+
+        expect(account).toBeTruthy();
+        expect(account.id).toBeTruthy();
+        expect(account.name).toBe('any_name');
+        expect(account.email).toBe('any_email@mail.com');
+        expect(account.password).toBe('any_password');
+    });
+});
